25d1b39137b41e76668ee7cad8d096f1d128369c
santi <santy.bolla@hotmail.com>
2020-04-12 20:45:25 -0300

    test

---
+++ test.js
@@ -0,0 +1,61 @@
+/*
+Ejercicio 1:
+imprimir todos los numeros entre 10 y 33
+*/
+
+}
+/*
+Ejercicio 2:
+imprimir todos los nombres en la lista
+*/
+const nombres = ['hernan', 'luis', 'gerardo', 'horacio']
+
+
+/*
+Ejercicio 3:
+
+un poco de matematica: el modulo de un numero es el numero sin el signo
+tambien se lo llama "valor absoluto". en javascript existe la funcion Math.abs()
+que recibe un numero y devuelve su valor absoluto. ejemplos:
+Math.abs(-7) = 7
+Math.abs(5) = 5
+
+el ejercicio: imprimir todos los numeros entre -20 y 20 cuyo valor absoluto
+sea mayor a 15
+*/
+
+/*
+Ejercicio 4:
+
+un poco de matematica: un numero es par si el resto de dividirlo por 2 es 0.
+repaso del resto:
+- si dividimos 12 por 5, tenemos un resto de 2
+- si dividimos 17 por 6 tenemos un resto de 5
+
+el resto de dividir por 2 puede ser 0 o 1 solamente. si es 0, corresponde a un
+numero par, si es 1 corresponde a un numero impar.
+
+en javascript se puede consultar el resto de una division de la siguiente forma:
+10 % 2 retorna el resto de dividir 10 por 2, que es 0
+17 % 2 retorna el resto de dividir 17 por 2, que es 1
+
+el ejercicio: imprimir todos los numeros pares entre 0 y 30
+*/
+
+/*
+Ejercicio 5:
+
+un poco de matematica: un numero X se dice que es divisible por otro numero Z
+cuando el resto de dividir a X por Z es 0.
+
+el ejercicio: imprimir todos los numeros entre 0 y 100 que sean divisibles por
+3 y por 5 (simultaneamente)
+*/
+
+/*
+Ejercicio 6:
+
+similar al ejercicio anterior, imprimir todos los numeros entre 0 y 50 que sean
+divisibles por 3 o por 5 (no necesariamente divisible por ambos, pero al menos por
+uno de los 2).
+*/
